656ccc6779c8b9499c9c252a72ba965cc92ff897
Dane Springmeyer <dane@mapbox.com>
2015-11-05 12:12:42 -0800

    properly release and drain pool

--- test/pool.js
+++ test/pool.js
@@ -23,7 +23,12 @@ test('mapnik-pool', function(t) {
         t.equal(map.bufferSize, 256, 'sets a buffer size');
         t.equal(map.width, 256, 'sets map size');
         t.equal(map.height, 256, 'sets map size');
-        t.end();
+        pool.release(map);
+        pool.drain(function() {
+            pool.destroyAllNow(function() {
+                t.end();
+            });
+        });
     });
 });
 
@@ -33,9 +38,15 @@ test('initOptions', function(t) {
             { size: 1024 });
 
     pool.acquire(function(err, map) {
+        t.equal(err, null, 'no error returned');
         t.equal(map.width, 1024, 'use initOptions to set map width');
         t.equal(map.height, 1024, 'use initOptions to set map height');
-        t.end();
+        pool.release(map);
+        pool.drain(function() {
+            pool.destroyAllNow(function() {
+                t.end();
+            });
+        });
     });
 });
 
@@ -44,7 +55,12 @@ test('passes errors', function(t) {
             { bufferSize: 256 });
 
     pool.acquire(function(err, map) {
-        t.ok(err instanceof Error);
-        t.end();
+        t.ok(err instanceof Error,'expected error');
+        t.ok(!map,'expected map to be null');
+        pool.drain(function() {
+            pool.destroyAllNow(function() {
+                t.end();
+            });
+        });
     });
 });
